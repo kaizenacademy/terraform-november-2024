72af737e5a4219e5e29625a86180e0e8a7ab5084
kaizenacademy <kaizen.techacademy@gmail.com>
2025-01-18 02:44:12 +0000

    new files

--- Class3/main.tf
+++ Class3/main.tf
@@ -7,14 +7,37 @@ resource "aws_key_pair" "deployer" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-08970251d20e940b0"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   #availability_zone = "us-east-2c"
   subnet_id = "subnet-059bab6cb0aa00fc7"
   key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+output ec2 {
+    value = aws_instance.web.public_ip
+    sensitive = true
 }
