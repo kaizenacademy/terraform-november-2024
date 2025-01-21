11b3398dc781fc4467c3400f4e496f38a65ec4f1
kaizenacademy <kaizen.techacademy@gmail.com>
2025-01-21 01:21:37 +0000

    fix

--- Class4/apache.sh
+++ Class4/apache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo apt update
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
