2ed26f63212660ccf37041bdce096554844051e4
kaizenacademy <kaizen.techacademy@gmail.com>
2025-01-23 03:05:39 +0000

    new changes

---
+++ Class5-backend/main.tf
@@ -0,0 +1,14 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Team = "DevOps"
+    Environment = "Dev"
+    Owner = "Adilet"
+  }
+}
