69478cfc1a78295fab934f582953fafe446799fe
kaizenacademy <kaizen.techacademy@gmail.com>
2025-01-25 00:21:52 +0000

    new change

---
+++ Class6/main.tf
@@ -0,0 +1,16 @@
+provider aws {
+    region = "us-east-2"
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+   provisioner "local-exec" {
+   command = "mkdir kaizen && touch hello"
+ }
+
+}
+
+
