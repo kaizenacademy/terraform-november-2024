d81af71aa38ebeeaf12c21a59a5a2cd9c7b95f75
kaizenacademy <kaizen.techacademy@gmail.com>
2025-01-25 00:39:56 +0000

    new change

--- Class6/main.tf
+++ Class6/main.tf
@@ -2,10 +2,27 @@ provider aws {
     region = "us-east-2"
 }
 
-resource "null_resource" "cluster" {
-   provisioner "local-exec" {
-   command = "mkdir kaizen && touch hello"
- }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0eb070c40e6a142a3"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install httpd -y"
+    ]
+  }
+}
+
+# resource "null_resource" "cluster" {
+#    provisioner "local-exec" {
+#    command = "mkdir kaizen && touch hello"
+#  }
+# }
 
 
